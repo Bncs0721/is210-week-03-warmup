4095053121c63651894e46cead4011c0aa1035c8
Biencis Rubiera <biencis@gmail.com>
2016-09-19 20:44:52 +0000

    Created file task_14.py and tested logical operator 'and' + equality

---
+++ task_14.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Practicing with True, False, AND, equality operator"""
+
+IS_TRUE = True
+
+print IS_TRUE
+
+IS_FALSE = False
+
+print IS_FALSE
+
+IS_NONE = None
+
+print None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
+
+print INTEGER_EQUIV
