57ef6e10f225f8cda4ef5594adb9ef3e7c7b6fef
Biencis Rubiera <biencis@gmail.com>
2016-09-19 03:19:11 +0000

    Added a multiline docstring to task_09.py

--- task_09.py
+++ task_09.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question. To complete this task, we
+have to test a docstring that should break across two paragraphs. Then
+this is one paragraph.
+
+And this is my second paragraph. The double quotations will 'tell' python
+where my docstring ends.
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
